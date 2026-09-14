546ae01f86857ee224daadd307a482da47d65ff2
Arthur de Sousa Machado <sousaarthur213@gmail.com>
2026-09-14 00:07:31 -0300

    Adiciona ArrayList de alunos no Main

--- src/Main.java
+++ src/Main.java
@@ -1,3 +1,5 @@
+import java.util.ArrayList;
+
 public class Main {
     public static void main(String[] args) {
         Aluno aluno1 = new Aluno();
@@ -6,7 +8,23 @@ public class Main {
         aluno1.adicionarNota(9.0);
         aluno1.adicionarNota(7.5);
 
+        Aluno aluno2 = new Aluno();
+        aluno2.setNome("João");
+        aluno2.setIdade(20);
+        aluno2.adicionarNota(4.0);
+        aluno2.adicionarNota(7.5);
+
+        ArrayList<Aluno> alunos = new ArrayList<>();
+        alunos.add(aluno1);
+        alunos.add(aluno2);
+
         System.out.println("Meu nome é: " + aluno1.getNome() + " e tenho " + aluno1.getIdade() + " anos");
         System.out.println("Notas: " + aluno1.getNotas());
+
+        System.out.println("Meu nome é: " + aluno2.getNome() + " e tenho " + aluno2.getIdade() + " anos");
+        System.out.println("Notas: " + aluno2.getNotas());
+
+        System.out.println("Tamanho da lista: " + alunos.size());
+
     }
 }
